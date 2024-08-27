54f2dbf4a4e3a0f164f17fbe4acddf2e9fb8902a
Ashtondup <ashtondup@gmail.com>
2024-08-27 16:44:39 +0200

    Hide password
    Added functionality to hide password when user enters the password

--- Main_Program.py
+++ Main_Program.py
@@ -1,4 +1,4 @@
-import Calculations as C, File_Handling as FH, Telescope_Movement as TM, re
+import Calculations as C, File_Handling as FH, Telescope_Movement as TM, re, getpass
 
 USER, PASSWORD = 'f', 'f'
 
@@ -232,7 +232,7 @@ def __main__():
     # Check login details
     while b_flag == False:
         user = input("Enter your username: ")
-        password = input("Enter your password: ")
+        password = getpass.getpass("Enter your password: ") # Hiddes password, does not echo the password as the user types.
 
         if (USER == user) & (PASSWORD == password):
             b_flag = True
